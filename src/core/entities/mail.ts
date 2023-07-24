6f1ac9791c3ddf1c4d56fc4230a3663e009f4733
migue-l-b <miguellopesbraido@gmail.com>
2023-07-24 11:33:36 -0300

    feat: create MailModel

--- src/core/entities/mail.ts
+++ src/core/entities/mail.ts
@@ -1,7 +1,10 @@
 import { z } from "zod"
 import MailSchema from "@core/entities/models/mail.ts"
 import AddressEmailSchema from "@core/entities/models/address-email.ts"
+import MailModelSchema from "@core/entities/models/mail-model.ts"
 
 export type Mail = z.infer<typeof MailSchema>
+
+export type MailModel = z.infer<typeof MailModelSchema>
 
 export type AddressEmail = z.infer<typeof AddressEmailSchema>
---
+++ src/core/entities/models/mail-model.ts
@@ -0,0 +1,12 @@
+import { z } from "zod"
+import FileSchema from "@core/entities/models/file.ts"
+
+const MailModelSchema = z.object({
+  id: z.string().uuid(),
+  subject: z.string().min(5).max(25),
+  body: z.string(),
+  isMD: z.boolean(),
+  files: z.array(FileSchema).optional(),
+})
+
+export default MailModelSchema
