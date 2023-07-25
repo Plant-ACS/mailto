7d2099f603808239e4f8ea361c62318f8a72fdeb
migue-l-b <miguellopesbraido@gmail.com>
2023-07-25 12:18:59 -0300

    conf: keeping code production cohesive

---
+++ src/controllers/use-cases/group/add-email.ts
@@ -0,0 +1,7 @@
+import IAddEmailInGroup from "@core/use-cases/group/add-email.ts"
+
+export default class AddEmailInGroup implements IAddEmailInGroup {
+  add(data: string): Promise<void> {
+		throw new Error("Method not implemented.")
+  }
+}
