55d63385b3196ad3af8b87aacb2b6535b3995679
Bruno <brunoconcli@gmail.com>
2023-08-05 16:29:03 -0300

    fix: Incorrect simple quote

--- src/controllers/use-cases/group/delete.ts
+++ src/controllers/use-cases/group/delete.ts
@@ -6,7 +6,7 @@ export default class DeleteGroup implements IDeleteGroup {
 	async delete(data: DeleteDTO): Promise<Group> {
 		return await GroupDB.findOneAndDelete({ name: data.name, subName: data.subName })
 			.then((group) => {
-				if (!group) throw new Error('Group not found')
+				if (!group) throw new Error("Group not found")
 				return group
 			})
 			.catch((err) => { throw new Error(err) })
