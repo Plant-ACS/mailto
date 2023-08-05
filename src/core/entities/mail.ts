7ccd13f9bc6d88bcdc28caf173dd99ad698d78ce
Bruno <brunoconcli@gmail.com>
2023-08-05 16:30:01 -0300

    fix: Missing ObjectId

--- src/core/entities/mail.ts
+++ src/core/entities/mail.ts
@@ -1,10 +1,11 @@
 import { z } from "zod"
+import { Types } from "mongoose"
 import MailSchema from "@core/entities/models/mail.ts"
 import AddressEmailSchema from "@core/entities/models/address-email.ts"
 import MailModelSchema from "@core/entities/models/mail-model.ts"
 
 export type Mail = z.infer<typeof MailSchema>
 
-export type MailModel = z.infer<typeof MailModelSchema>
+export type MailModel = z.infer<typeof MailModelSchema> & Types.ObjectId
 
 export type AddressEmail = z.infer<typeof AddressEmailSchema>
