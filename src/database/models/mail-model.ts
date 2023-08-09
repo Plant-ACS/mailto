d1137d460d9d284dbe79a8fc5032e3d9d68e298d
Bruno Concli <brunoconcli@gmail.com>
2023-08-09 15:28:19 -0300

    fix: Readd Id property

--- src/database/models/mail-model.ts
+++ src/database/models/mail-model.ts
@@ -3,6 +3,10 @@ import getConnection from "@database/connection.ts"
 const { Schema, model } = await getConnection()
 
 const MailModelSchema = new Schema({
+	_id: {
+		type: String,
+		required: true,
+	},
 	subject: {
 		type: String,
 		required: true,
