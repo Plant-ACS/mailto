8ad451f037a7ec4f9d7e7aad7fef593e604d2e0c
Bruno Concli <brunoconcli@gmail.com>
2023-08-09 15:29:15 -0300

    fix: Incorrect Promise parameter

--- src/controllers/use-cases/mail-model/delete.ts
+++ src/controllers/use-cases/mail-model/delete.ts
@@ -1,13 +1,21 @@
 import IDeleteMailModel from "@core/use-cases/mail-model/delete.ts";
 import { DeleteMailModelDTO } from "@core/use-cases/mail-model/delete.ts";
 import MailModelDB from "@database/models/mail-model.ts";
+import { MailModel } from "@core/entities/mail.ts";
 
 export default class DeleteMailModel implements IDeleteMailModel {
-	async delete(data: DeleteMailModelDTO): Promise<void> {
+	async delete(data: DeleteMailModelDTO): Promise<MailModel> {
 		return await MailModelDB.findByIdAndDelete({data})
 			.then((mailmodel)=> {
 				if (!mailmodel) throw new Error("Mail Model not found")
-				return mailmodel
+				const mailModel: MailModel = {
+					_id: mailmodel._id.toString(),
+					subject: mailmodel.subject,
+					body: mailmodel.body,
+					isMD: mailmodel.isMD,
+					files: mailmodel.files
+				}
+				return mailModel
 			})
 			.catch((err) => { throw new Error(err) })
 	}
