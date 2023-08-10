e0d05b02878e93041766d4675b10b407d997a75d
Bruno Concli <brunoconcli@gmail.com>
2023-08-10 16:58:06 -0300

    fix: Incorrect return statement

--- src/controllers/use-cases/mail-model/delete.ts
+++ src/controllers/use-cases/mail-model/delete.ts
@@ -1,22 +1,10 @@
 import IDeleteMailModel from "@core/use-cases/mail-model/delete.ts";
 import { DeleteMailModelDTO } from "@core/use-cases/mail-model/delete.ts";
 import MailModelDB from "@database/models/mail-model.ts";
-import { MailModel } from "@core/entities/mail.ts";
-
 export default class DeleteMailModel implements IDeleteMailModel {
-	async delete(data: DeleteMailModelDTO): Promise<MailModel> {
-		return await MailModelDB.findByIdAndDelete({data})
-			.then((mailmodel)=> {
-				if (!mailmodel) throw new Error("Mail Model not found")
-				const mailModel: MailModel = {
-					_id: mailmodel._id.toString(),
-					subject: mailmodel.subject,
-					body: mailmodel.body,
-					isMD: mailmodel.isMD,
-					files: mailmodel.files
-				}
-				return mailModel
-			})
+	async delete(data: DeleteMailModelDTO): Promise<void> {
+		await MailModelDB.findByIdAndDelete({data})
+			.then((mailmodel)=> { if (!mailmodel) throw new Error("Mail Model not found")})
 			.catch((err) => { throw new Error(err) })
 	}
 }
